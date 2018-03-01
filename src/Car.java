836c17df69377f7b244c5cea55c0c9b1715a3035
Adrian Mucha <kestarumper8@gmail.com>
2018-03-01 21:31:12 +0100

    abcd

---
+++ src/Car.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Car {
+    private int pos_x;
+    private int pos_y;
+    private int nextFree = 0;
+    public int nextArrive = 0;
+
+    ArrayList<Road> roadsHandled = new ArrayList<>();
+
+    void moveToPosition(int x, int y) {
+        this.pos_x = x;
+        this.pos_y = y;
+    }
+
+    int distanceTo(int x, int y) {
+        return Math.abs(this.pos_x - x) + Math.abs(this.pos_y - y);
+    }
+
+    int distanceTo(int car_x, int car_y, int dest_x, int dest_y) {
+        return Math.abs(car_x - dest_x)+Math.abs(car_y - dest_y);
+    }
+
+    boolean canHandle(Road road) {
+        int toCustomer = distanceTo(road.start_pos_x, road.end_pos_y);
+        int fromCustomerToDestination = distanceTo(road.start_pos_x, road.start_pos_y, road.end_pos_x, road.end_pos_y);
+
+        nextArrive = nextFree + toCustomer + fromCustomerToDestination;
+
+        return nextFree + toCustomer + fromCustomerToDestination <= road.maxEnd;
+    }
+
+    public void setNextFree(int nextFree) {
+        this.nextFree = nextFree;
+    }
+
+    void addRoad(Road road) {
+        this.roadsHandled.add(road);
+    }
+
+    public Car(int pos_x, int pos_y) {
+        this.pos_x = pos_x;
+        this.pos_y = pos_y;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "roadsHandled=" + roadsHandled +
+                '}';
+    }
+}
---
+++ src/Road.java
@@ -0,0 +1,32 @@
+public class Road {
+    private final int id;
+    public int start_pos_x;
+    public int start_pos_y;
+    public int end_pos_x;
+    public int end_pos_y;
+    public final int minStart;
+    public final int maxEnd;
+
+    public Road(int start_pos_x, int start_pos_y, int end_pos_x, int end_pos_y, int minStart, int maxEnd, int id) {
+        this.start_pos_x = start_pos_x;
+        this.start_pos_y = start_pos_y;
+        this.end_pos_x = end_pos_x;
+        this.end_pos_y = end_pos_y;
+        this.minStart = minStart;
+        this.maxEnd = maxEnd;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Road{" +
+                "ID=" + id +
+                ", start_pos_x=" + start_pos_x +
+                ", start_pos_y=" + start_pos_y +
+                ", end_pos_x=" + end_pos_x +
+                ", end_pos_y=" + end_pos_y +
+                ", minStart=" + minStart +
+                ", maxEnd=" + maxEnd +
+                '}';
+    }
+}
